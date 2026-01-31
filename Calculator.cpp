38c4a947243bdfee435986ffd0d1cb2c6a751993
Halina0585 <usmaanshafi11@gmail.com>
2026-01-31 19:21:05 +0530

    Adding basic functions

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cmath>
 
 int printMenu();
+int mode(int menuType);
 void basicMath();
 
 int main()
 {
 
-    int choice = printMenu();
+    int menuType = printMenu();
 
-    switch (choice)
+    switch (menuType)
     {
     case 1:
         basicMath();
@@ -24,7 +26,7 @@ int main()
 
 int printMenu()
 {
-    int choice = 1;
+    int choice = 0;
     std::cout << "\nThis is a CLI Calculator written in C++\n";
     std::cout << "\nPlease select what mode you would like to enter\n";
     std::cout << "Basic arthematic operations [1]\n";
@@ -35,44 +37,90 @@ int printMenu()
     return choice;
 }
 
+int mode(int menuType)
+{
+    int mode;
+
+    switch (menuType)
+    {
+    case 1:
+        std::cout << "\nSelect an Operation\n";
+        std::cout << "[1] Addition\n";
+        std::cout << "[2] Subtraction\n";
+        std::cout << "[3] Multipication\n";
+        std::cout << "[4] Division\n";
+        std::cout << "[5] Exponent\n";
+        std::cout << "[6] Root\n";
+        std::cout << "[7] Percentage\n";
+        std::cin >> mode;
+        break;
+
+    default:
+        std::cout << "Error: Invalid Mode Selected!";
+        break;
+    }
+
+    return mode;
+}
+
 void basicMath()
 {
 
     double num1;
     double num2;
-    char op;
+    int operation = mode(1);
 
-    std::cout << "First number: ";
+    std::cout << "\nFirst number: ";
     std::cin >> num1;
-
-    std::cout << "Operation[+,-,*,/]: ";
-    std::cin >> op;
 
     std::cout << "Secound number: ";
     std::cin >> num2;
 
-    if (op == '+')
+    switch (operation)
     {
+    case 1: // Addition
         std::cout << "Result = ", num1 + num2;
         std::cout << num1 + num2;
-    }
-    else if (op == '-')
-    {
+        break;
+
+    case 2: // Subtraction
         std::cout << "Result = ";
         std::cout << num1 - num2;
-    }
-    else if (op == '*')
-    {
+        break;
+
+    case 3: // Multiplication
+
         std::cout << "Result = ";
         std::cout << num1 * num2;
-    }
-    else if (op == '/')
-    {
+        break;
+
+    case 4: // Division
+        if (num2 == 0)
+        {
+            std::cout << "Error: Cannot Divide by Zero!";
+            break;
+        }
         std::cout << "Result = ";
         std::cout << num1 / num2;
-    }
-    else
-    {
-        std::cout << "Invalid Operation!";
+        break;
+
+    case 5: // Exponent
+        std::cout << "Results = ";
+        std::cout << pow(num1, num2);
+        break;
+
+    case 6: // Root
+        std::cout << "Results = ";
+        std::cout << pow(num1, (1 / num2));
+        break;
+
+    case 7: // Percentage
+        std::cout << "result = ";
+        std::cout << num1 * (num2 / 100);
+        break;
+
+    default:
+        std::cout << "Error: Invalid Operation!";
+        break;
     }
 }
