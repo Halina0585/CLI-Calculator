6784c7ab9a4426e8f80352de3b3305656e41d219
Halina0585 <usmaanshafi11@gmail.com>
2026-03-04 22:09:59 +0530

    removed the useless shit, now its just a basic calculator

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,86 +2,14 @@
 #include <cmath>
 
 int printMenu();
-int mode(int menuType);
-void basicMath();
 
 int main()
 {
-
-    int menuType = printMenu();
-
-    switch (menuType)
-    {
-    case 1:
-        basicMath();
-        break;
-
-    default:
-        std::cout << "Invalid Mode!";
-        break;
-    }
-
-    return 0;
-}
-
-int printMenu()
-{
-    int choice = 0;
-    std::cout << "\nThis is a CLI Calculator written in C++\n";
-    std::cout << "\nPlease select what mode you would like to enter\n";
-    std::cout << "Basic arthematic operations [1]\n";
-    std::cout << "Area and Volumes [2]\n";
-    std::cout << "----> ";
-    std::cin >> choice;
-
-    return choice;
-}
-
-int mode(int menuType)
-{
-    int mode;
-
-    switch (menuType)
-    {
-    case 1: // Basic Athermatic
-        std::cout << "\nSelect an Operation\n";
-        std::cout << "[1] Addition\n";
-        std::cout << "[2] Subtraction\n";
-        std::cout << "[3] Multipication\n";
-        std::cout << "[4] Division\n";
-        std::cout << "[5] Exponent\n";
-        std::cout << "[6] Root\n";
-        std::cout << "[7] Percentage\n";
-        std::cout << "----> ";
-        std::cin >> mode;
-        break;
-
-    case 2: // Area
-        std::cout << "[1] Rectriangle\n";
-        std::cout << "[2] Triangle\n";
-        std::cout << "[3] Circle\n";
-        std::cout << "[4] Trapezium\n";
-        std::cout << "[5] Parallelogram\n";
-        std::cout << "[6] Kite\n";
-        std::cout << "[7] Area\n";
-        std::cout << "[8] Area\n";
-        std::cout << "[9] Area\n";
-        break;
-
-    default:
-        std::cout << "Error: Invalid Mode Selected!";
-        break;
-    }
-
-    return mode;
-}
-
-void basicMath()
-{
-
     double num1;
     double num2;
-    int operation = mode(1);
+    int operation;
+
+    operation = printMenu();
 
     std::cout << "\nFirst number: ";
     std::cin >> num1;
@@ -136,4 +64,23 @@ void basicMath()
         std::cout << "Error: Invalid Operation!";
         break;
     }
+
+    return 0;
+}
+
+int printMenu()
+{
+    int operation;
+    std::cout << "\nSelect an Operation\n";
+    std::cout << "[1] Addition\n";
+    std::cout << "[2] Subtraction\n";
+    std::cout << "[3] Multipication\n";
+    std::cout << "[4] Division\n";
+    std::cout << "[5] Exponent\n";
+    std::cout << "[6] Root\n";
+    std::cout << "[7] Percentage\n";
+    std::cout << "----> ";
+    std::cin >> operation;
+
+    return operation;
 }
